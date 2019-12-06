8547cd05d68be9a60a58e54ef5d8fa474128c7f2
jinshu1987 <jinshu.allumkal@gmail.com>
2019-12-06 22:11:55 +0000

    Update particles.js

--- particles.js
+++ particles.js
@@ -10,7 +10,7 @@ particlesJS("particles-js", {
       }
     },
     "color": {
-      "value": "#000099"
+      "value": "#ffffff"
     },
     "shape": {
       "type": "circle",
@@ -50,7 +50,7 @@ particlesJS("particles-js", {
     "line_linked": {
       "enable": true,
       "distance": 150,
-      "color": "#000099",
+      "color": "#ffffff",
       "opacity": 0.4,
       "width": 1
     },
